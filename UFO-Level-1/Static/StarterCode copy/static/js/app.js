dd49742ad5670b8450f2cd206caf7c81706223e8
Dominique <niquedunning@Dominiques-MacBook-Pro-2.local>
2020-10-16 08:49:18 -0700

    changes to js code

--- UFO-Level-1/Static/StarterCode copy/static/js/app.js
+++ UFO-Level-1/Static/StarterCode copy/static/js/app.js
@@ -1,61 +1,113 @@
-var Data = data
-
 // from data.js
+var tableData = data;
 
-var tbody = d3.select("tbody")
+// Using d3 to select the table
+var table = d3.select('table');
+
+// Using d3 to select the table body
+var tbody = d3.select('tbody');
 
 
-function dataShow(item){
-    tbody.text("");
-    item.forEach(ufo => {
-        console.table(ufo);
-        add_tr = tbody.append("tr");
+// Using d3 to append one table row `tr` for each sighting object
+function buildTable(data) {
+    
+    data.forEach((ufo) => {
 
-        Object.entries(ufo).forEach(function([key,value]){
-            add_td = add_tr.append("td").text(value);
-        });
+    var row = tbody.append("tr");
+
+    // Using `Object.entries` to console.log each sighting's value
+    Object.entries(ufo).forEach(([key, value]) => {
+       var cell = row.append("td").text(value);
     });
-}
+})};
 
-dataShow(Data)
-console.log("Second Step")
+buildTable(tableData);
 
-var Button = d3.select("#filter-btn")
-Button.on("click", function(){
-    console.log("Step 3")
+var dropdown = d3.select('#dropdown');
+dropdown.on('click', function() {
+    
+    // Select the dropdown
+    var dropdownoptions = d3.select('#dropdown').node().value;
 
-d3.event.preventDefault();
+    d3.select('#criteria').node().value = '';
 
-var dateInput = d3.select("#datetime");
-var dateValue = dateInput.property("value");
+    switch(dropdownoptions) {
+        case 'date':
+            place = '1/11/2010';
+            break;
 
-var cityInput = d3.select("#city");
-var cityValue = cityInput.property("value");
+        case 'city':
+            place = 'Benton';
+            break;
 
-var stateInput = d3.select("#state");
-var stateValue = stateInput.property("value");
+        case 'state':
+            place = 'Ca';
+            break;
+        
+        case 'country':
+            place = 'US';
+            break;
+        
+        case 'shape':
+            place = 'Circle';
+            break;
+        
+        case 'selection':
+            place = '';
+            break;
+    }
+    d3.select('#criteria').attr('placeholder', place);
+    d3.select('label').attr('for',dropdownoptions).text(`Enter a value for ${dropdownoptions}: `);
 
-var countryInput = d3.select("#country");
-var countryValue = countryInput.property("value");
-
-var shapeInput = d3.select("#shape");
-var shapeValue = shapeInput.property("value");
-
-
-console.log(dateValue);
-console.log(cityValue);
-console.log(stateValue);
-console.log(countryValue);
-console.log(shapeValue);
-
-var Filter = Data.filter(ufo =>{
-    return(ufo.datetime===dateValue || !dateValue) &&
-              (ufo.city===cityValue || !cityValue) &&
-              (ufo.state===stateValue || !stateValue) &&
-              (ufo.country===countryValue || !countryValue) &&
-              (ufo.shape===shapeValue || !shapeValue )
 })
 
-dataDisplay(Filter);
+// Select the button
+var button = d3.select('button');
+// console.log(button);
 
-});
+// Select the form
+var form = d3.select('form');
+// console.log(form);
+
+// Create event handler 
+button.on("click", runEnter);
+
+// Complete the event handler function for the form
+function runEnter() {
+    
+    // Prevent the page from refreshing
+    d3.event.preventDefault();
+    
+    // Select the dropdown
+    var dropdownselection = d3.select('#dropdown').node().value;
+
+    // Select the input element and get the raw HTML node
+    var inputElement = d3.select("#criteria");
+  
+    // Get the value property of the input element
+    var inputValue = inputElement.property("value").toLowerCase();
+    // console.log(inputValue);
+
+    function ufofilter(ufo){
+        if (dropdownselection === 'date' && ufo.datetime === inputValue){
+            return ufo.datetime;
+        } else if (dropdownselection === 'city' && ufo.city === inputValue){
+            return ufo.city;
+        } else if (dropdownselection === 'state' && ufo.state === inputValue){
+            return ufo.state;
+        } else if (dropdownselection === 'country' && ufo.country === inputValue){
+            return ufo.country;
+        } else if (dropdownselection === 'shape' && ufo.shape === inputValue){
+            return ufo.shape;
+        }
+    }
+
+    var filteredData = tableData.filter(ufofilter);
+    // console.log(filteredData);
+
+    // clearing out all table rows
+    tbody.html('');
+
+    // append filtered date to the table
+    buildTable(filteredData);
+}
